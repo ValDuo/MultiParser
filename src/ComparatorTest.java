28269b3cc2926bc26b81f195f3c4d1a4845ca92b
d.kostukov <dimon20040611@gmail.com>
2024-12-04 11:35:44 +0300

    Modified ComparatorSentRecieved

---
+++ src/ComparatorTest.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class ComparatorTest {
+    public static void main(String[] args) throws IOException {
+        String input_file = "C:\\Users\\d.kostukov\\IdeaProjects\\parcing-of-kadastrs\\srcFiles\\new_kadastr_28_10_2024.csv";
+        String output_directory = "C:\\Users\\d.kostukov\\IdeaProjects\\parcing-of-kadastrs\\srcFiles\\zip1_test";
+        ComparatorSentRecieved comparator = new ComparatorSentRecieved(input_file,output_directory);
+        comparator.compare();
+    }
+}
