10a81129e8057d669208d4a48706561aa24f72e0
Valeriia Dubina <dvdlera@gmail.com>
2024-12-10 11:04:15 +0300

    fix of warnings

--- src/ProcessThread.java
+++ src/ProcessThread.java
@@ -1,11 +1,16 @@
 import org.openqa.selenium.WebDriver;
 
 public class ProcessThread extends Thread{
+    private boolean isActive = true;
     private final CSV_IO csv;
     private final WebDriver driver;
+    void disable() {
+        this.isActive = false;
+    }
     ProcessThread(CSV_IO srcFile, WebDriver driver){
         super();
         this.csv = srcFile;
+        this.isActive = true;
         this.driver = driver;
     }
 
