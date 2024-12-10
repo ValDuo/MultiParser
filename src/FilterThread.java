9c30d88403c28705541d0fe43b033d2b87058c8d
d.kostukov <dimon20040611@gmail.com>
2024-12-10 11:37:27 +0300

    Modified ComparatorSentRecieved

--- src/FilterThread.java
+++ src/FilterThread.java
@@ -1,21 +1,25 @@
+import java.io.IOException;
+
 public class FilterThread extends Thread{
     private boolean isActive;
+    private String responsePath;
+    private String requestPath;
     void disable(){
         isActive= false;
     }
-    FilterThread(){
+    FilterThread(String requestPath, String responsePath){
         isActive = true;
+        this.requestPath = requestPath;
+        this.responsePath = responsePath;
+
     }
     @Override
     public void run() {// Этот метод будет вызван при старте потока
-        int i = 0;//счетчик циклов
-        while (isActive) { // Вывод в цикле с паузой 0.5 сек
-            System.out.println("Поток для фильтрации!" + i++);
-            try {
-                sleep(500); // Задержка в 0.5 сек
-            } catch (Exception e) {
-                System.out.println("Поток был прерван!");
-            }
+        try {
+            ComparatorSentRecieved comparator = new ComparatorSentRecieved(requestPath, responsePath);
+            comparator.compare();
+        } catch (IOException e) {
+            System.out.println(e);
         }
     }
 }
